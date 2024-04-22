606e4abb130074fed91003504391037f8d07b6cd
robelEth <74147588+robelEth@users.noreply.github.com>
2024-04-22 18:22:50 -0400

    Add files via upload

---
+++ kaleidoscope_arduino_IMU/kaleidoscope_arduino_IMU.ino
@@ -0,0 +1,28 @@
+#include "ICM_20948.h"
+ICM_20948_I2C myICM;
+
+void setup() {
+ Serial.begin(115200);
+ Wire.begin();
+ Wire.setClock(400000);
+ myICM.begin(Wire, 1);
+}
+
+void loop() {
+    // Getting IMU data
+ if ( myICM.dataReady() ) {
+   myICM.getAGMT();
+   float ax = myICM.accX();
+   float ay = myICM.accY();
+   float az = myICM.accZ();
+   int force = analogRead(0);
+
+   Serial.print(ax, 1);
+   Serial.print(", ");
+   Serial.print(ay, 1);
+   Serial.print(", ");
+   Serial.print(az, 1);
+   Serial.println();
+ }
+ delay(20);
+}
